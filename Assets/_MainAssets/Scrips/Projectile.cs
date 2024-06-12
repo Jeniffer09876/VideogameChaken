2805fcba5a271c89e1e419ab5314c01674007d2e
karol <mayorquinmariana5@gmail.com>
2024-06-11 21:39:00 -0500

    Reapply "particles impact system"
    This reverts commit 85f485b001de20c36acfb4a1f08f203e758bb563.

--- Assets/_MainAssets/Scrips/Projectile.cs
+++ Assets/_MainAssets/Scrips/Projectile.cs
@@ -9,6 +9,8 @@ public class Projectile : MonoBehaviour
     public float speed;
     private float sampleTime;
     public bool magic;
+    public ParticleSystem boomParticles;
+    public ParticleSystem magicParticles;
 
     // Start is called before the first frame update
     void Start()
@@ -38,11 +40,22 @@ private void OnEnable()
 
         if (sampleTime > 1f)
         {
-            Debug.Log("boom");
-            gameObject.SetActive(false);
+           // Debug.Log("boom");
+            //boomParticles.Play(true);
+            magicParticles.Stop(true);
             magic = false;
             sampleTime = 0f;
           
         }
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("boom");
+        boomParticles.Play(true);
+        magicParticles.Stop(true);
+//gameObject.SetActive(false);
+    }
+
+  
 }
